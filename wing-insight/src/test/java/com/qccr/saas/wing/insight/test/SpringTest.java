55f442be3d9558ffcd451259a05ad3f1cd76e683
夏飞 <xiafei@qccr.com>
2018-01-31 20:05:33 +0800

    Mock测试全面通过

--- wing-insight/src/test/java/com/qccr/saas/wing/insight/test/SpringTest.java
+++ wing-insight/src/test/java/com/qccr/saas/wing/insight/test/SpringTest.java
@@ -1,7 +1,7 @@
 package com.qccr.saas.wing.insight.test;
 
+import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
@@ -10,5 +10,9 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 public class SpringTest {
 
 
+    @Test
+    public void test(){
+
+    }
 
 }
