7c8939119eccf440089b31f141787683e5fd4d56
xia-fei <xiafei.dev@gmail.com>
2018-11-01 17:00:48 +0800

    版本更新

--- wing-insight/src/test/java/com/wing/insight/test/ConfigTest.java
+++ wing-insight/src/test/java/com/wing/insight/test/ConfigTest.java
@@ -1,14 +1,8 @@
 package com.wing.insight.test;
 
-import com.wing.insight.config.ConfigMain;
-import org.junit.Test;
-
 /**
  * @author 夏飞
  */
 public class ConfigTest {
-    @Test
-    public void testConfigMain(){
-        ConfigMain configMain=new ConfigMain("beacon");
-    }
+
 }
